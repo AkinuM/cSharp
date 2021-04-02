f00ad4208d8a9094f86cd0dd8bd41d905ec42676
AkinuM <73168415+AkinuM@users.noreply.github.com>
2021-04-02 15:26:17 +0300

    Add files via upload

---
+++ Lab4/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace inputAndOutput
+{
+	class Program
+	{
+		[DllImport("inputAndOutput.dll", CallingConvention = CallingConvention.StdCall)]
+		private static extern double add(double numb1, double numb2);
+
+		[DllImport("inputAndOutput.dll", CallingConvention = CallingConvention.StdCall)]
+		private static extern double substract(double numb1, double numb2);
+
+		static void Main(string[] args)
+		{
+			double qwe;
+			qwe = add(4.2, 5.8);
+			Console.WriteLine(qwe);
+			qwe = substract(4.2, 5.8);
+			Console.WriteLine(qwe);
+		}
+	}
+}
