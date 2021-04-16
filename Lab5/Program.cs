355fb60ef85279e6e3f61353c55bc719aa3e3217
AkinuM <73168415+AkinuM@users.noreply.github.com>
2021-04-16 15:55:22 +0300

    Add files via upload

---
+++ Lab5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dotaPlayer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			pos5DotaPlayer fsdf = new pos5DotaPlayer();
+			Console.WriteLine($"{fsdf.Role}");
+			Console.WriteLine($"{fsdf.MMR}");
+			fsdf.playGame("Lina", "Middle");
+			Console.WriteLine($"{fsdf.MMR}");
+			fsdf.playGame("Lina", "Middle");
+			Console.WriteLine($"{fsdf.MMR}");
+			pos1DotaPlayer qwe = new pos1DotaPlayer();
+			qwe.playGame("Lina", "Middle");
+			pos3DotaPlayer ert = new pos3DotaPlayer();
+			ert.playGame("Lina", "Middle");
+		}
+	}
+}
