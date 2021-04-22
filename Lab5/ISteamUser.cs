21b5cc5deeb13c4a5d42bec8644c9e9903878018
AkinuM <73168415+AkinuM@users.noreply.github.com>
2021-04-22 11:15:36 +0300

    Add files via upload

---
+++ Lab5/ISteamUser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace dotaPlayer
+{
+	interface ISteamUser
+	{
+		void ChangeProfileName();
+
+		string ProfileName { get; }
+
+		void ChangeRealName();
+
+		string RealName { get; }
+
+		void ChangeCustomUrl();
+
+		string CustomUrl { get; }
+
+		void ChangeCountry();
+
+		string Country { get; }
+
+		void ChangeSummary();
+
+		string Summary { get; }
+
+		void ChangeEmailAddress();
+
+		string EmailAddress { get; }
+
+		void ChangePhone();
+
+		string Phone { get; }
+
+		void ChangePassword();
+
+		string Password { get; }
+
+		void VerifyEmailAddress();
+
+		bool EmailAddressStatus { get; }
+
+		void VerifySteamGuard();
+
+		bool SteamGuardStatus { get; }
+
+		void AddFriend();
+
+		string this[int index] { get; }
+
+		void Deposit(int amount);
+
+		int Wallet { get; }
+
+		void Deposit(string expression);
+	}
+}
